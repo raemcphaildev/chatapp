9bd0134361b727618f3904221c8808c48b575d51
rae.mcphail <rae.mcphail@ucalgary.ca>
2020-11-11 16:03:31 -0700

    saves genaric cookies

--- index.js
+++ index.js
@@ -3,13 +3,40 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var express = require('express');
+var cookieParser = require('cookie-parser');
 
+io = require('express')();
+io.use(cookieParser());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+  console.log("Cookies: ", req.cookies)
+  res.cookie ("user", "bob", { maxAge: 60 * 60 * 1000});
+  res.cookie ("colour", "red", { maxAge: 60 * 60 * 1000});
+  res.cookie ("user", "jerry", { maxAge: 60 * 60 * 1000});
   res.sendFile(__dirname + '/index.html');
 });
 
+// //JSON object to be added to cookie 
+// let users = { 
+//   name : "Ritik", 
+//   Age : "18"
+// } 
+    
+//   //Route for adding cookie 
+// app.get('/setuser', (req, res)=>{ 
+//   res.cookie("userData", users); 
+//   res.send('user data added to cookie'); 
+// }); 
+
+// //Iterate users data from cookie 
+// app.get('/getuser', (req, res)=>{ 
+//   //shows all the cookies 
+//   res.send(req.cookies); 
+// });
+
 io.on('connection', (socket) => {
+  
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
